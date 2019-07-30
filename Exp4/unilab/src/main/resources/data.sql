24d533775c1189569d5e8479c3392262d323e873
Mateus Alexandre <mateusalxd@gmail.com>
2019-07-30 14:01:18 -0300

    Carga de dados para testes

--- Exp4/unilab/src/main/resources/data.sql
+++ Exp4/unilab/src/main/resources/data.sql
@@ -1,3 +1,12 @@
 insert into aluno (data_nascimento, nome, endereco, matricula, telefone) values ('2019-01-01', 'Aluno 1', 'Rua 1, do Aluno 1', 'A0000001', '19-11111-1111');
 insert into aluno (data_nascimento, nome, endereco, matricula, telefone) values ('2019-01-02', 'Aluno 2', 'Rua 2, do Aluno 2', 'A0000002', '19-22222-2222');
 insert into aluno (data_nascimento, nome, endereco, matricula, telefone) values ('2019-01-03', 'Aluno 3', 'Rua 3, do Aluno 3', 'A0000003', '19-33333-3333');
+
+insert into perfil (nome) values('alunos_delete');
+insert into perfil (nome) values('alunos_get');
+insert into perfil (nome) values('alunos_post');
+insert into perfil (nome) values('alunos_put');
+insert into perfil (nome) values('perfis_delete');
+insert into perfil (nome) values('perfis_get');
+insert into perfil (nome) values('perfis_post');
+insert into perfil (nome) values('perfis_put');
