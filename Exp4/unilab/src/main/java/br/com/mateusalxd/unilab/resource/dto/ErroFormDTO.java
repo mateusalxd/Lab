a7ace90cb50734e7c4490ba2101acd85c09d66d4
Mateus Alexandre <mateusalxd@gmail.com>
2019-07-30 19:07:08 -0300

    Remoção de anotações desnecessárias

--- Exp4/unilab/src/main/java/br/com/mateusalxd/unilab/resource/dto/ErroFormDTO.java
+++ Exp4/unilab/src/main/java/br/com/mateusalxd/unilab/resource/dto/ErroFormDTO.java
@@ -1,14 +1,9 @@
 package br.com.mateusalxd.unilab.resource.dto;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-
-@ApiModel(description = "Visualização utilizada na exibição de um Erro de Formulário")
 public class ErroFormDTO {
 
-	@ApiModelProperty(value = "Nome do campo", example = "nome")
 	private String campo;
-	@ApiModelProperty(value = "Mensagem de erro", example = "O campo nome é obrigatório")
+
 	private String mensagem;
 
 	public ErroFormDTO(String campo, String mensagem) {
