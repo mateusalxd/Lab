018d4261bbd551dd50ccbbf1e6ddb7482793babd
Mateus Alexandre <mateusalxd@gmail.com>
2019-08-01 12:59:10 -0300

    Dados iniciais

--- Exp4/unilab/src/main/resources/data.sql
+++ Exp4/unilab/src/main/resources/data.sql
@@ -1,31 +1,32 @@
-insert into aluno (data_nascimento, nome, endereco, matricula, telefone) values ('2019-01-01', 'Aluno 1', 'Rua 1, do Aluno 1', 'A0000001', '19-11111-1111');
-insert into aluno (data_nascimento, nome, endereco, matricula, telefone) values ('2019-01-02', 'Aluno 2', 'Rua 2, do Aluno 2', 'A0000002', '19-22222-2222');
-insert into aluno (data_nascimento, nome, endereco, matricula, telefone) values ('2019-01-03', 'Aluno 3', 'Rua 3, do Aluno 3', 'A0000003', '19-33333-3333');
-
+insert into perfil (nome) values('*_*');
+insert into perfil (nome) values('actuator_*');
 insert into perfil (nome) values('alunos_delete');
 insert into perfil (nome) values('alunos_get');
 insert into perfil (nome) values('alunos_post');
 insert into perfil (nome) values('alunos_put');
+insert into perfil (nome) values('cursos_delete');
+insert into perfil (nome) values('cursos_get');
+insert into perfil (nome) values('cursos_post');
+insert into perfil (nome) values('cursos_put');
+insert into perfil (nome) values('disciplinas_delete');
+insert into perfil (nome) values('disciplinas_get');
+insert into perfil (nome) values('disciplinas_post');
+insert into perfil (nome) values('disciplinas_put');
 insert into perfil (nome) values('perfis_delete');
 insert into perfil (nome) values('perfis_get');
 insert into perfil (nome) values('perfis_post');
 insert into perfil (nome) values('perfis_put');
+insert into perfil (nome) values('professores_delete');
+insert into perfil (nome) values('professores_get');
+insert into perfil (nome) values('professores_post');
+insert into perfil (nome) values('professores_put');
+insert into perfil (nome) values('usuarios_delete');
+insert into perfil (nome) values('usuarios_get');
+insert into perfil (nome) values('usuarios_post');
+insert into perfil (nome) values('usuarios_put');
 
-insert into curso (nome) values('Ciência da Computação');
-insert into curso (nome) values('Direito');
+insert into professor (data_nascimento, nome, cargo, codigo, salario) values ('2019-01-01', 'Administrador', 'Administrador do Sistema', 'admin', 0.0);
 
-insert into professor (data_nascimento, nome, cargo, codigo, salario) values ('1980-01-03', 'Professor 1', 'Cargo 1', 'C0000001', 1000.00);
-insert into professor (data_nascimento, nome, cargo, codigo, salario) values ('1990-07-25', 'Professor 2', 'Cargo 2', 'C0000002', 1999.99);
-insert into professor (data_nascimento, nome, cargo, codigo, salario) values ('2000-01-03', 'Professor 3', 'Cargo 3', 'C0000003', 1000.00);
+insert into usuario (ativo, bloqueado, data_cadastro, nome, senha) values (1, 0, NOW(), 'admin', '$2a$10$HYL3W7b1W2BfbW3ZEJrTke.Fe4GbwoZMqCT.c2C.luutbTIJyryc2');
 
-insert into disciplina (nome) values('Calculo I');
-insert into disciplina (nome) values('Calculo II');
-insert into disciplina (nome) values('Ética');
-
-insert into usuario (ativo, bloqueado, data_cadastro, nome, senha) values (1, 0, NOW(), 'A0000001', '$2a$10$HYL3W7b1W2BfbW3ZEJrTke.Fe4GbwoZMqCT.c2C.luutbTIJyryc2');
-insert into usuario (ativo, bloqueado, data_cadastro, nome, senha) values (1, 0, NOW(), 'A0000002', '$2a$10$HYL3W7b1W2BfbW3ZEJrTke.Fe4GbwoZMqCT.c2C.luutbTIJyryc2');
-insert into usuario (ativo, bloqueado, data_cadastro, nome, senha) values (1, 0, NOW(), 'A0000003', '$2a$10$HYL3W7b1W2BfbW3ZEJrTke.Fe4GbwoZMqCT.c2C.luutbTIJyryc2');
-
-insert into usuario_perfis (usuario_id, perfis_id) values (1, 2);
-insert into usuario_perfis (usuario_id, perfis_id) values (2, 2);
-insert into usuario_perfis (usuario_id, perfis_id) values (3, 2);
+insert into usuario_perfis (usuario_id, perfis_id) values (1, 1);
