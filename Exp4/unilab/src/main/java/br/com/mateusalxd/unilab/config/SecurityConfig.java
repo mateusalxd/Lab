4e7cf3c048fbb99633008b46c29d08b41923da38
Mateus Alexandre <mateusalxd@gmail.com>
2019-08-02 12:31:55 -0300

    Correções de perfil

--- Exp4/unilab/src/main/java/br/com/mateusalxd/unilab/config/SecurityConfig.java
+++ Exp4/unilab/src/main/java/br/com/mateusalxd/unilab/config/SecurityConfig.java
@@ -47,38 +47,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.antMatchers(HttpMethod.POST, "/autenticacao").permitAll()
-			.antMatchers("/**").hasAuthority("*_*")
+			.antMatchers("/h2-console", "/h2-console/**").permitAll()
+			
 			.antMatchers("/actuator/**").hasAuthority("actuator_*")
 			
-			.antMatchers(HttpMethod.DELETE, "/alunos/*").hasAuthority("alunos_delete")
-			.antMatchers(HttpMethod.GET, "/alunos", "/alunos/*").hasAuthority("alunos_get")
-			.antMatchers(HttpMethod.POST, "/alunos").hasAuthority("alunos_post")
-			.antMatchers(HttpMethod.PUT, "/alunos/*").hasAuthority("alunos_put")
+			.antMatchers(HttpMethod.DELETE, "/alunos/*").hasAnyAuthority("alunos_delete", "*_*")
+			.antMatchers(HttpMethod.GET, "/alunos", "/alunos/*").hasAnyAuthority("alunos_get", "*_*")
+			.antMatchers(HttpMethod.POST, "/alunos").hasAnyAuthority("alunos_post", "*_*")
+			.antMatchers(HttpMethod.PUT, "/alunos/*").hasAnyAuthority("alunos_put", "*_*")
 			
-			.antMatchers(HttpMethod.DELETE, "/cursos/*").hasAuthority("cursos_delete")
-			.antMatchers(HttpMethod.GET, "/cursos", "/cursos/*").hasAuthority("cursos_get")
-			.antMatchers(HttpMethod.POST, "/cursos").hasAuthority("cursos_post")
-			.antMatchers(HttpMethod.PUT, "/cursos/*").hasAuthority("cursos_put")
+			.antMatchers(HttpMethod.DELETE, "/cursos/*").hasAnyAuthority("cursos_delete", "*_*")
+			.antMatchers(HttpMethod.GET, "/cursos", "/cursos/*").hasAnyAuthority("cursos_get", "*_*")
+			.antMatchers(HttpMethod.POST, "/cursos").hasAnyAuthority("cursos_post", "*_*")
+			.antMatchers(HttpMethod.PUT, "/cursos/*").hasAnyAuthority("cursos_put", "*_*")
 			
-			.antMatchers(HttpMethod.DELETE, "/disciplinas/*").hasAuthority("disciplinas_delete")
-			.antMatchers(HttpMethod.GET, "/disciplinas", "/disciplinas/*").hasAuthority("disciplinas_get")
-			.antMatchers(HttpMethod.POST, "/disciplinas").hasAuthority("disciplinas_post")
-			.antMatchers(HttpMethod.PUT, "/disciplinas/*").hasAuthority("disciplinas_put")		
+			.antMatchers(HttpMethod.DELETE, "/disciplinas/*").hasAnyAuthority("disciplinas_delete", "*_*")
+			.antMatchers(HttpMethod.GET, "/disciplinas", "/disciplinas/*").hasAnyAuthority("disciplinas_get", "*_*")
+			.antMatchers(HttpMethod.POST, "/disciplinas").hasAnyAuthority("disciplinas_post", "*_*")
+			.antMatchers(HttpMethod.PUT, "/disciplinas/*").hasAnyAuthority("disciplinas_put", "*_*")
 			
-			.antMatchers(HttpMethod.DELETE, "/perfis/*").hasAuthority("perfis_delete")
-			.antMatchers(HttpMethod.GET, "/perfis", "/perfis/*").hasAuthority("perfis_get")
-			.antMatchers(HttpMethod.POST, "/perfis").hasAuthority("perfis_post")
-			.antMatchers(HttpMethod.PUT, "/perfis/*").hasAuthority("perfis_put")				
+			.antMatchers(HttpMethod.DELETE, "/perfis/*").hasAnyAuthority("perfis_delete", "*_*")
+			.antMatchers(HttpMethod.GET, "/perfis", "/perfis/*").hasAnyAuthority("perfis_get", "*_*")
+			.antMatchers(HttpMethod.POST, "/perfis").hasAnyAuthority("perfis_post", "*_*")
+			.antMatchers(HttpMethod.PUT, "/perfis/*").hasAnyAuthority("perfis_put", "*_*")		
 			
-			.antMatchers(HttpMethod.DELETE, "/professores/*").hasAuthority("professores_delete")
-			.antMatchers(HttpMethod.GET, "/professores", "/professores/*").hasAuthority("professores_get")
-			.antMatchers(HttpMethod.POST, "/professores").hasAuthority("professores_post")
-			.antMatchers(HttpMethod.PUT, "/professores/*").hasAuthority("professores_put")	
+			.antMatchers(HttpMethod.DELETE, "/professores/*").hasAnyAuthority("professores_delete", "*_*")
+			.antMatchers(HttpMethod.GET, "/professores", "/professores/*").hasAnyAuthority("professores_get", "*_*")
+			.antMatchers(HttpMethod.POST, "/professores").hasAnyAuthority("professores_post", "*_*")
+			.antMatchers(HttpMethod.PUT, "/professores/*").hasAnyAuthority("professores_put", "*_*")
 			
-			.antMatchers(HttpMethod.DELETE, "/usuarios/*").hasAuthority("usuarios_delete")
-			.antMatchers(HttpMethod.GET, "/usuarios", "/usuarios/*").hasAuthority("usuarios_get")
-			.antMatchers(HttpMethod.POST, "/usuarios").hasAuthority("usuarios_post")
-			.antMatchers(HttpMethod.PUT, "/usuarios/*").hasAuthority("usuarios_put")	
+			.antMatchers(HttpMethod.DELETE, "/usuarios/*").hasAnyAuthority("usuarios_delete", "*_*")
+			.antMatchers(HttpMethod.GET, "/usuarios", "/usuarios/*").hasAnyAuthority("usuarios_get", "*_*")
+			.antMatchers(HttpMethod.POST, "/usuarios").hasAnyAuthority("usuarios_post", "*_*")
+			.antMatchers(HttpMethod.PUT, "/usuarios/*").hasAnyAuthority("usuarios_put", "*_*")
+			
+			.antMatchers("/**").hasAuthority("*_*")
 			
 			.anyRequest().denyAll()
 			.and().csrf().disable()
@@ -92,7 +95,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
+		web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**", "/h2-console/**");
 	}
 	
 }
