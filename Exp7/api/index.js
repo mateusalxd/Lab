e8dd3c226a5ae8ae6221076f9c05aada2fb98565
Mateus Alexandre <mateusalxd@gmail.com>
2019-08-18 20:53:48 -0300

    Inclusão do CORS

--- Exp7/api/index.js
+++ Exp7/api/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql');
 const schema = require('./graphql/schema');
+const cors = require('cors');
 
 var app = express();
+app.use(cors());
 app.use('/graphql',
     graphqlHTTP({
         schema,
         graphiql: true
     }));
 app.listen(4000, () => {
-    console.log('Exeuctando em localhost:4000/graphql')
-})
+    console.log('Exeuctando em http://localhost:4000/graphql');
+});
